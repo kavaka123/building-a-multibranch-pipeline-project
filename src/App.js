f102066d22180c9fc93919be7ffcae0ba042e40d
kavaka123 <avant.aditya@gmail.com>
2020-02-08 16:50:45 +0530

    Added new line in App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <br>
+          I'm testing this line
+          </br>
         </p>
       </div>
     );
